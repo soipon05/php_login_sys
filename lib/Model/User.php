bc3f3395799e422e2a17b00613c7bf1f85d12445
JUN <soipon0@gmail.com>
2019-05-09 23:44:22 +0900

    index.phpの作成

--- lib/Model/User.php
+++ lib/Model/User.php
@@ -4,9 +4,10 @@ namespace MyApp\Model;
 
 class User extends \MyApp\Model
 {
+
     public function create($values)
     {
-        $stmt = $this->db->prepare("insert into users (email, password, created. modified) values (:email, :password, now(), now())");
+        $stmt = $this->db->prepare("insert into users (email, password, created, modified) values (:email, :password, now(), now())");
         $res = $stmt->execute([
             ':email' => $values['email'],
             ':password' => password_hash($values['password'], PASSWORD_DEFAULT)
@@ -14,5 +15,25 @@ class User extends \MyApp\Model
         if ($res === false) {
             throw new \MyApp\Exception\DuplicateEmail();
         }
+    }
+
+    public function login($values)
+    {
+        $stmt = $this->db->prepare("select * from users where email = :email");
+        $stmt->execute([
+            ':email' => $values['email']
+        ]);
+        $stmt->setFetchMode(\PDO::FETCH_CLASS, 'stdClass');
+        $user = $stmt->fetch();
+
+        if (empty($user)) {
+            throw new \MyApp\Exception\UnmatchEmailOrPassword();
+        }
+
+        if (!password_verify($values['password'], $user->password)) {
+            throw new \MyApp\Exception\UnmatchEmailOrPassword();
+        }
+
+        return $user;
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,9 +1,38 @@
 <?php
 
-// ユーザー一覧
+// ユーザーの一覧
 
 require_once(__DIR__ . '/../config/config.php');
 
-$app = new MyApp\Controller\Index();
+// var_dump($_SESSION['me']);
 
-$app->run();
+// $app = new MyApp\Controller\Index();
+//
+// $app->run();
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div id="container">
+        <form action="logout.php" method="post" id="logout">
+            taguchi@gmail.com <input type="submit" value="Log Out">
+            <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+        </form>
+        <h1>Users <span class="fs12">(3)</span></h1>
+        <ul>
+            <li>taguchi@gmail.com</li>
+            <li>taguchi@gmail.com</li>
+            <li>taguchi@gmail.com</li>
+        </ul>
+    </div>
+</body>
+
+</html>
