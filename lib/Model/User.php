79edb153994c65381cf93462566677b375cf1cf2
JUN <soipon0@gmail.com>
2019-05-09 22:32:36 +0900

    Loginコントローラーの作成

--- lib/Model/User.php
+++ lib/Model/User.php
@@ -5,5 +5,14 @@ namespace MyApp\Model;
 class User extends \MyApp\Model
 {
     public function create($values)
-    { }
+    {
+        $stmt = $this->db->prepare("insert into users (email, password, created. modified) values (:email, :password, now(), now())");
+        $res = $stmt->execute([
+            ':email' => $values['email'],
+            ':password' => password_hash($values['password'], PASSWORD_DEFAULT)
+        ]);
+        if ($res === false) {
+            throw new \MyApp\Exception\DuplicateEmail();
+        }
+    }
 }
