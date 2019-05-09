5d8f376a797bf8d094b37cd950db258f2dc2fc40
JUN <soipon0@gmail.com>
2019-05-09 21:56:45 +0900

    Modelクラスの作成

---
+++ lib/Model.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MyApp;
+
+class Model
+{
+    protected $db;
+
+    public function __construct()
+    {
+        try {
+            $this->db = new \PDO(DSN, DB_USERNAME, DB_PASSWORD);
+        } catch (\PDOException $e) {
+            echo $e->getMEssage();
+            exit;
+        }
+    }
+}
---
+++ lib/Model/User.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MyApp\Model;
+
+class User extends \MyApp\Model
+{
+    public function create($values)
+    { }
+}
